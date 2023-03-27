7c0f17ae602a95874af8f8e042fd56d6c120714f
Helen89 <mulen_rush89@mail.ru>
2023-03-27 15:41:14 +0700

    TestZero

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -102,7 +102,7 @@ public class TodosTest {
         Task[] actual = todos.search("Выкатка 3й версии приложения");
         Assertions.assertArrayEquals(expected, actual);
     }
-    
+
     @Test
     public void shouldFindTwoTasks() {
         SimpleTask simpleTask = new SimpleTask(5, "Хлеб");
@@ -127,4 +127,29 @@ public class TodosTest {
         Task[] actual = todos.search("Хлеб");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void shouldFindZeroTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { };
+        Task[] actual = todos.search("Сварить компот");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
